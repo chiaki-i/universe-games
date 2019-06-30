45c81dfd9a4bc275df7a44da8e9cdd054cd01def
Chiaki ISHIO <chiaki.ishio@gmail.com>
2019-07-01 00:13:01 +0900

    cannot move after gameover, add disk5 mode

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# universeJs-games
+# universe-games
+
+- play: [Tower of Hanoi (3 disks)](https://chiaki-i.github.io/universe-games/hanoi/game.html)
--- hanoi/game.ml
+++ hanoi/game.ml
@@ -13,17 +13,20 @@ type disk_t = {
   left_top : float * float;     (* 左上の座標 *)
   prev     : (float * float) option;
                                 (* 一つ前の座標 *)
-  size     : float;             (* 輪の大きさ 1-4 *)
+  size     : float;             (* 輪の大きさ1,2,3 *)
   bar      : int;               (* 軸 左から1,2,3 *)
   pos      : float;             (* 軸のどの高さにあるか *)
   moved    : int;               (* この輪を動かした回数 *)
   color    : Color.t;
 }
 
+type mode_t = Disk3 | Disk5
+
 type world_t = {
   disks : disk_t list;          (* 輪 *)
   move  : int;                  (* 動かすことができる既定の回数 *)
   remaining : int;              (* 残りの回数 *)
+  mode  : mode_t;               (* 輪の枚数 *)
   msg   : string;               (* debugging *)
 }
 
@@ -39,22 +42,21 @@ let bar_bot = 350.
 
 (* 軸の定義 *)
 let bars = [
-  (rectangle 100. 20. sandyBrown); (rectangle 20. 200. sandyBrown);
-  (rectangle 100. 20. sandyBrown); (rectangle 20. 200. sandyBrown);
-  (rectangle 100. 20. sandyBrown); (rectangle 20. 200. sandyBrown);
+  (rectangle 160. 20. sandyBrown); (rectangle 20. 200. sandyBrown);
+  (rectangle 160. 20. sandyBrown); (rectangle 20. 200. sandyBrown);
+  (rectangle 160. 20. sandyBrown); (rectangle 20. 200. sandyBrown);
 ]
 
 (* 軸の位置 *)
 let bars_posn = [
-  (50., 350.);  (90., 150.);
-  (250., 350.); (290., 150.);
-  (450., 350.); (490., 150.);
+  (20., 350.);  (90., 150.);
+  (220., 350.); (290., 150.);
+  (420., 350.); (490., 150.);
 ]
 
 (* 軸を含めた背景 *)
 let background =
   place_images bars bars_posn (empty_scene width height)
-
 
 let make_left_top (bar : int) (pos : float) (size : float) : (float * float) =
   let current_bar =
@@ -98,14 +100,30 @@ let draw world = match world with {disks = d; remaining = m; msg = msg} ->
          (rectangle 55. 25. gray80)
          (5., 5.)      
          with_moves) in
+  let with_disk3_button =
+    place_image
+      (text "3" gray10)
+      (71., 5.)
+      (place_image
+         (rectangle 20. 25. gray80)
+         (68., 5.)      
+         with_reset_button) in
+  let with_disk5_button =
+    place_image
+      (text "5" gray10)
+      (98., 5.)
+      (place_image
+         (rectangle 20. 25. gray80)
+         (95., 5.)      
+         with_disk3_button) in
   let with_msg =
     place_image
       (text msg gray10)
       (width /. 2., 0.)
-      with_reset_button in
+      with_disk5_button in
   with_msg
 
-let initial_world = {
+let initial_world_3 = {
   disks = [
     make_disk 1. 1 3. 0 gray20;
     make_disk 2. 1 2. 0 gray50;
@@ -113,6 +131,21 @@ let initial_world = {
   ];
   move = 10;
   remaining = 10;
+  mode = Disk3;
+  msg = "";
+}
+
+let initial_world_5 = {
+  disks = [
+    make_disk 1. 1 5. 0 gray10;
+    make_disk 2. 1 4. 0 gray30;
+    make_disk 3. 1 3. 0 gray50;
+    make_disk 4. 1 2. 0 gray70;
+    make_disk 5. 1 1. 0 gray90;
+  ];
+  move = 35;
+  remaining = 35;
+  mode = Disk5;
   msg = "";
 }
 
@@ -140,38 +173,53 @@ let on_reset_button (x : float) (y : float) : bool =
   then true
   else false
 
+let on_disk3_button (x : float) (y : float) : bool =
+  if (68. <= x) && (x <= 88.) && (5. <= y) && (y <= 30.)
+  then true
+  else false
+
+let on_disk5_button (x : float) (y : float) : bool =
+  if (95. <= x) && (x <= 105.) && (5. <= y) && (y <= 30.)
+  then true
+  else false
+
 (***** マウス処理 *****)
 let end_with (world : world_t) : world_t =
   match world with {disks = ds; move = m} ->
   match fetch_bar_info world 3 with {height = h3} ->
+  match fetch_bar_info world 2 with {height = h2} ->
     let total_moved =
       List.fold_left (+) 0 (List.map (fun {moved = mvd} -> mvd) ds) in
-    if h3 = 3. then {world with remaining = m - total_moved;
-                                    msg = "; )"}
+    if (h3 = float_of_int (List.length ds)) || (* 一番右の軸に完成させた、もしくは *)
+       (h2 = float_of_int (List.length ds))    (* 真ん中の軸に完成させた *)
+    then {world with remaining = m - total_moved; msg = "; )"}
     else
       begin
-        if total_moved = 10                 (* 既定回数に到達 *)
+        if total_moved = m                 (* 既定回数に到達 *)
         then {world with remaining = m - total_moved;
                          msg = "try again"}
-        else if total_moved > 10 then world (* 既定回数以上は操作回数が減らない *)
+        else if total_moved > m then world (* 既定回数以上は操作回数が減らない *)
         else {world with remaining = m - total_moved}
       end
          
 let block_button_down (world : world_t) (x : float) (y : float) (d : disk_t) : disk_t =
   let current_bar = on_which_bar x y in
-  match fetch_bar_info world current_bar with
-  | {disk_sizes = sizes} ->
-    let min_size = List.fold_left (min) max_float sizes in
-    match d with {left_top = (left, top); size = s} ->
-      let right = left +. (s *. disk_module) in
-      let bottom = top +. disk_module in
-      if (left <= x) && (x <= right) && (top <= y) && (y <= bottom) && (s <= min_size)
-      then        (* ブロックの中心座標 = マウスの座標 かつ 一番小さい（一番上の）ブロック *)
-        let new_left = x -. (s *. disk_module /. 2.) in
-        let new_top = y -. (disk_module /. 2.) in
-        {d with prev = Some (left, top);
-                left_top = (new_left, new_top)}
-      else d
+  match world with {remaining = remaining} ->
+    if remaining = 0 then d
+    else
+      match fetch_bar_info world current_bar with
+      | {disk_sizes = sizes} ->
+        let min_size = List.fold_left (min) max_float sizes in
+        match d with {left_top = (left, top); size = s} ->
+          let right = left +. (s *. disk_module) in
+          let bottom = top +. disk_module in
+          if (left <= x) && (x <= right) && (top <= y) && (y <= bottom) && (s <= min_size)
+          then        (* ブロックの中心座標 = マウスの座標 かつ 一番小さい（一番上の）ブロック *)
+            let new_left = x -. (s *. disk_module /. 2.) in
+            let new_top = y -. (disk_module /. 2.) in
+            {d with prev = Some (left, top);
+                    left_top = (new_left, new_top)}
+          else d
 
 let block_button_up (world : world_t) (x : float) (y : float) (d : disk_t) : disk_t =
   match d with
@@ -186,7 +234,7 @@ let block_button_up (world : world_t) (x : float) (y : float) (d : disk_t) : dis
     | {height = h; disk_sizes = sizes} ->
       let top_size = List.fold_left (max) 0. sizes in
       if (prev_bar <> current_bar) &&                 (* 元の軸と違う軸で手を離した *)
-         (current_bar <> 0) &&                        (* いずれかの軸の上で手を離した *)
+         (current_bar <> 0) &&                        (* いずれかの軸の上(=背景ではない)で手を離した *)
          ((disk_size <= top_size) ||                  (* その軸の最小の輪より小さい、または *)
           (top_size = 0.))                            (* 軸にまだ１つも輪がない *)
       then
@@ -199,9 +247,14 @@ let block_button_up (world : world_t) (x : float) (y : float) (d : disk_t) : dis
                    prev = None}
 
 let on_mouse_button_down (world : world_t) (x : float) (y : float) : world_t =
-  if on_reset_button x y then initial_world
-  else 
-    match world with {disks = ds; msg = msg} ->
+  match world with {mode = current_mode; disks = ds; msg = msg} -> 
+    if on_reset_button x y then
+      (if current_mode = Disk3 then initial_world_3 else initial_world_5)
+    else if on_disk3_button x y then
+      (if current_mode = Disk3 then world else initial_world_3)
+    else if on_disk5_button x y then
+      (if current_mode = Disk5 then world else initial_world_5)
+    else
       {world with disks = List.map (block_button_down world x y) ds}
 
 let on_mouse_button_up (world : world_t) (x : float) (y : float) : world_t =
@@ -217,7 +270,7 @@ let on_mouse (world : world_t) (x : float) (y : float) (mouse_event : string)
   | _ -> {world with msg = "(" ^ (string_of_float x) ^ ", " ^ (string_of_float y) ^ ")"}
 
 let _ =
-  big_bang initial_world
+  big_bang initial_world_3
     ~name:    "ToH"
     ~to_draw: draw
     ~width:   (int_of_float width)
